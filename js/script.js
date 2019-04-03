8bd722adeae1c652440fa098d4a5f0557bc98ce7
Lebegue <lebegue.guillaume26@gmail.com>
2019-04-03 15:43:15 +0200

    add link etc

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     anime({
         targets: '.staggering-grid-demo .el',
         scale: [
@@ -9,29 +9,90 @@ $(document).ready(function(){
     });
 
 });
-function Dropdown(){
 
-        document.getElementById('nav').style.visibility='visible';
-        document.getElementById('nav-shop').style.visibility='visible';
+function Dropdown() {
+
+    document.getElementById('nav').style.visibility = 'visible';
+    document.getElementById('nav-shop').style.visibility = 'visible';
 
 
 }
-function changeShoe(color){
-    if(color=='black'){
-        $('.shoe3d').hide()
-        $('.shoemodel').show()
-        document.getElementById('shoe').src="img/shoe1.png"
-    }else if(color=='white'){
-        $('.shoe3d').hide()
-        $('.shoemodel').show()
-        document.getElementById('shoe').src="img/shoe2.png"
-    }else if(color=='purple'){
-        $('.shoe3d').hide()
-        $('.shoemodel').show()
-        document.getElementById('shoe').src="img/shoe3.png"
-    }else{
-        $('.shoemodel').hide()
-        $('.shoe3d').show()
 
+function changeShoe(color) {
+    if (color === 'black') {
+
+        $('.shoe3d').hide()
+        $('.shoemodel').fadeIn(600);
+
+
+        $('#shoe').attr('src', 'img/shoe1.png')
+
+
+    } else if (color === 'white') {
+
+        $('.shoe3d').hide()
+        $('.shoemodel').fadeIn(600);
+
+        $('#shoe').attr('src', 'img/shoe2.png')
+
+
+    } else if (color === 'purple') {
+
+        $('.shoe3d').hide();
+        $('.shoemodel').fadeIn(600);
+
+        $('#shoe').attr('src', 'img/shoe3.png')
+
+
+    } else {
+        $('.shoemodel').hide();
+
+        $('.shoe3d').fadeIn(600);
+
+
+    }
+}
+/*cart*/
+var cartOpen = false;
+var numberOfProducts = 0;
+
+$('body').on('click', '.js-toggle-cart', toggleCart);
+$('body').on('click', '.js-add-product', addProduct);
+$('body').on('click', '.js-remove-product', removeProduct);
+
+function toggleCart(e) {
+    e.preventDefault();
+    if(cartOpen) {
+        closeCart();
+        return;
+    }
+    openCart();
+}
+
+function openCart() {
+    cartOpen = true;
+    $('body').addClass('open');
+}
+
+function closeCart() {
+    cartOpen = false;
+    $('body').removeClass('open');
+}
+
+function addProduct(e) {
+    e.preventDefault();
+    openCart();
+    $('.js-cart-empty').addClass('hide');
+    var product = $('.js-cart-product-template').html();
+    $('.js-cart-products').prepend(product);
+    numberOfProducts++;
+}
+
+function removeProduct(e) {
+    e.preventDefault();
+    numberOfProducts--;
+    $(this).closest('.js-cart-product').hide(250);
+    if(numberOfProducts == 0) {
+        $('.js-cart-empty').removeClass('hide');
     }
 }
